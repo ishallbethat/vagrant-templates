33b147f0d145175ff6e8220f41b9d091805a0eaf
Gabriel Wu <yingpingwdz@163.com>
2018-05-10 08:08:25 +0800

    add es

---
+++ elasticsearch/installOpenJDK8.sh
@@ -0,0 +1,6 @@
+apt-get update
+apt-get install default-jdk -y
+curl -L -O https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.2.4.tar.gz
+tar -xvf elasticsearch-6.2.4.tar.gz
+cd elasticsearch-6.2.4/bin
+nohup ./elasticsearch -Ecluster.name=es_cluster -Enode.name=es_node 1>es.log 2> es.err.log &
